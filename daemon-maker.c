bda2390dfcfbd53ec5e4c470de31379ef85e3fda
bddicken <bddicken@gmail.com>
2013-08-15 22:09:54 -0700

    added more features

--- daemon-maker.c
+++ daemon-maker.c
@@ -7,7 +7,7 @@
 int opt = 0;
 int start_daemon = 0;
 int daemon_time = 0;
-int interval = 0;
+int interval = 5;
 char *command = "";
 char working_dir[1024];
 
@@ -23,32 +23,43 @@ Simply pass a command to the `dm` execuatble with the `-c` flag, set an interval
 using the `-i` flag, and set a time limit using the `-t` flag. #boom. \n \
 \n \
 options: \n \
--h [y/n] : Show this help message, then exit \n \
--c [command] : Shell command to daemonize (in quotes) \n \
--i [time In seconds] : the interval to execute the daemon at. \n \
--t [time In seconds] : the amout of time to keep the daemon running for. \n \
+-h : Show this help message, then exit \n \
+-c [command] : Shell command to daemonize (in quotes). (default = "") \n \
+-i [time In seconds] : the interval to execute the daemon at. (default = 5). \n \
+-t [time In seconds] : the amout of time to keep the daemon running for. (default = 0 = infinity)\n \
  \n \
 "
 
+/**
+ * main - do I gotta explain?
+ */
 int main (int argc, char **argv)
 {
     parseargs(argc, argv);
                
     getworkingdir();
+    
+    printf("<<< daemon starting >>>\n");
 
     daemonize();
                 
     return EXIT_SUCCESS;    
 }
 
+/**
+ * Get the current working directory of the binary runner.
+ */
 void getworkingdir ()
 {
     char *res = getcwd(working_dir, sizeof(working_dir));
     if(res == NULL)
         exit(EXIT_FAILURE);
-    printf("working dir set to: %s\n", working_dir);
+    printf("<<< working dir set to: %s >>>\n", working_dir);
 }
 
+/**
+ * Turn the bash command passed to the program into a daemon.
+ */
 void daemonize ()
 {
     pid_t pid, sid;
@@ -78,55 +89,58 @@ void daemonize ()
     while(1)
     {
         system(command);
-
-        curr = time(&curr);
-        int time = (long) difftime(curr, start);
-        if(time > daemon_time)
-            break;
+   
+        /* If daemon_time == 0, then run forever. Else, run for daemon_time seconds */ 
+        if(daemon_time > 0) {
+            curr = time(&curr);
+            int time = (long) difftime(curr, start);
+            if(time > daemon_time)
+                break;
+        }
 
         sleep(interval);
     }
 
     exit(EXIT_SUCCESS);
-
 }
 
+/**
+ * Parse the command line arguments.
+ */
 void parseargs (int argc, char **argv)
 {
     char *str = NULL;
 
-    while ((opt = getopt(argc, argv, "i:c:t:h:")) != -1)
+    while ((opt = getopt(argc, argv, "i:c:t:h")) != -1)
     {
         switch (opt) {
             case 'h':
-                if(*optarg == 'y') {
-                    printf(HELP);
-                    exit(EXIT_SUCCESS);
-                }
+                printf(HELP);
+                exit(EXIT_SUCCESS);
                 break;
 
             case 'c':
                 command = malloc(strlen(optarg)+1);
                 strcpy(command, optarg);
-                printf("c:%s\n", optarg);
+                printf("<<< c = \'%s\' >>>\n", optarg);
                 break;
             
             case 'i':
-                printf("i:%s\n", optarg);
+                printf("<<< i = %s >>>\n", optarg);
                 str = malloc(strlen(optarg)+1);
                 strcpy(str, optarg);
                 interval = atoi(str);
                 break;
             
             case 't':
-                printf("t:%s\n", optarg);
+                printf("<<< t = %s >>>\n", optarg);
                 str = malloc(strlen(optarg)+1);
                 strcpy(str, optarg);
                 daemon_time = atoi(str);
                 break;
             
             default:
-                printf("invalid argument\n");
+                printf("<<< invalid argument, exiting >>>\n");
                 exit(EXIT_FAILURE);
                 break;
         }
